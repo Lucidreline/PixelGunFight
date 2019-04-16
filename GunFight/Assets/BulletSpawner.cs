821df823cc6fb07e1ddee6213623e520b6d189dd
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-04-16 09:40:04 -0700

    Player one can now shoot bullets out of his gun using a seperate 'bullet spawner' game object that is a child to the player

---
+++ GunFight/Assets/BulletSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] float bulletSpeed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fire();
+    }
+
+    private void fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            var bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(bulletSpeed, 0);
+        }
+    }
+}
