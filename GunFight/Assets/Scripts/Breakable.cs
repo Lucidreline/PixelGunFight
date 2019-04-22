209076982a6fd5b2529e4f687d6877af66de6e21
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-04-22 14:42:55 -0700

    Created a script that I have added to varies scripts which allows them to be destroyed after being shot after a certain ammount of times. I have also created a very rough prefab of the star boxes.

---
+++ GunFight/Assets/Scripts/Breakable.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    [SerializeField] int shotsUntilBroken = 3;
+    int timesShot;
+
+    private void OnCollisionEnter2D(Collision2D collider)
+    {
+        if(collider.gameObject.tag == "Bullet")
+        {
+            timesShot++;
+            if(timesShot >= shotsUntilBroken)
+            {
+                Destroy(gameObject);
+            }
+            
+        }
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
