7d1c58aefd163d1e0fe923d49fa0e21b38a1dad5
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-04-23 11:34:44 -0700

    The coins now randomly spawn on the top and fall down. Right now the colors are off so that i can make sure that it is spawn each of them randomly but later i will make them look nicer. Also i added a coin wall so that the coins can no touch the player, bu the player is not affected by this wall. Although there is a small bug where the coins spawns right on top of the payer. Also i messed with the 2d physics matrix colliders so that the bullet can spawn closer to the player and not be destroyed instantly

---
+++ GunFight/Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    [SerializeField] float coinDropFrequency;
+    [SerializeField] List<GameObject> coinPrefabs;
+    [SerializeField] float freqMax = 2f;
+    [SerializeField] float freqMin = 5f;
+    float dropXPos;
+    
+    
+    bool coinLoop = true;
+    
+    void Update()
+    {
+        if (coinLoop) StartCoroutine(DropRandomCoin());
+    }
+
+    IEnumerator DropRandomCoin()
+    {
+        coinLoop = false;
+        coinDropFrequency = Random.Range(freqMin, freqMax);
+
+        yield return new WaitForSeconds(coinDropFrequency);
+
+        Vector2 dropPos = new Vector2 (Random.Range(-5f, 5f), 8 );
+        int RandomIndex = Random.Range(0, coinPrefabs.Count);
+        GameObject coinPrefab = coinPrefabs[RandomIndex];
+        Instantiate(coinPrefab, dropPos, Quaternion.identity);
+        coinLoop = true;
+    }
+
+}
