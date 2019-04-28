3391050036bfd803ecbb7ccb88b63464493f4285
Manuel Castaneda <Ma52Castaneda@gmail.com>
2019-04-28 11:47:38 -0700

    Created a basic path from the main menu to the game. Main menu scenes change smotthly with buttons. I also created materials for Text Mesh pro so that i can have difrerent size text outlines.

---
+++ GunFight/Assets/Scripts/menuScreenManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuScreenManager : MonoBehaviour
+{
+    [SerializeField] List<GameObject> menuScreens;
+    //menu screen index list
+    //0 = mainMenu
+    //1 = Game Modes
+    //2 = Controls
+
+    void Start()
+    {
+        SwitchMenuScreen(0);
+    }
+    
+    public void SwitchMenuScreen(int indexNum)
+    {
+        SwitchOffAllMenuScreens();
+        menuScreens[indexNum].SetActive(true);
+    }
+
+    void SwitchOffAllMenuScreens()
+    {
+        foreach(GameObject menuScreen in menuScreens)
+        {
+            menuScreen.SetActive(false);
+        }
+    }
+
+    
+    
+}
